c0e2c85aa5ab07773d14f138e0158a1ae0cae404
Noby Fujioka <n.fujioka@abacusmaths.info>
2019-01-20 22:55:24 +0000

    use promise instead of async await

--- handler.js
+++ handler.js
@@ -1,22 +1,22 @@
-'use strict';
-const {
-  getPage, parsePage, saveRatingsToDB
-} = require('./utils');
+"use strict";
+const { getPage, parsePage, saveRatingsToDB } = require("./utils");
 
 module.exports.scrape = (event, context, callback) => {
-  const scrapePage = async () => {
-    // 1. fetch yelp page
-    const page = await getPage(event);
-    // 2. parse the page
-    const yelpData = await parsePage(page);
+  // 1. fetch yelp page
+  getPage(event)
+  // 2. parse the page
+    .then(page => parsePage(page))
     // 3. save ratings data to our db
-    try {
-      await saveRatingsToDB(yelpData, event);
-    } catch(err) {
-      throw new Error(`Error scraping ${event}: ${JSON.stringify(err)}`);
-    }
-  };
-  scrapePage();
-  // adding return or callback to this handler.js will prevent dynamoDB to save the data for some reason..
-  // for now, I will not add return or callback to this handler.js.
+    .then(yelpData => saveRatingsToDB(yelpData, event))
+    .then(() =>
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: `Scraped ${event}`
+        })
+      })
+    )
+    .catch(error =>
+      callback(new Error(`Error scraping ${event}: ${JSON.stringify(error)}`))
+    );
 };
